d6ced5ab3b196a9087340e85341038c7c5982eed
teerawat65 <teerawatb65@nu.ac.th>
2024-11-18 16:45:19 +0700

    .

--- hello.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  int num1 =10;
-  int num2 =3;
-  
-  double div =num1/num2;
-
-  print("The division is,$div");
-}
---
+++ remainder.dart
@@ -0,0 +1,10 @@
+void main() {
+  int dividend = 10;
+  int divisor = 3;
+
+  int quotient = dividend ~/ divisor;
+  int remainder = dividend % divisor;
+
+  print('quotient = $quotient');
+  print('remainder = $remainder');
+}
