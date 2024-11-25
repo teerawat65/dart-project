729e380d0af91f6b7790395faa4dd636bd7181bd
teerawat65 <teerawatb65@nu.ac.th>
2024-11-25 13:52:17 +0700

    .

--- share.dart
+++ share.dart
@@ -1,5 +1,12 @@
 import 'dart:io';
 
-void main(){
+void main() {
+  print("กรุณากรอกค่าอาหารทั้งหมด:");
+  double totalCost = double.parse(stdin.readLineSync()!);
+
+  print("กรุณากรอกจำนวนคนทาน:");
+  int numberOfPeople = int.parse(stdin.readLineSync()!);
   
+  double costPerPerson = totalCost / numberOfPeople;
+  print("ค่าอาหารต่อคนคือ: ${costPerPerson.toStringAsFixed(2)} บาท");
 }
